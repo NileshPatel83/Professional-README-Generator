123cc3113ad46b9a36d951858269845be0eedee7
NileshPatel83 <hselin_83@yahoo.com>
2023-02-16 11:34:30 +1000

    variables created to access different modules.

---
+++ index.js
@@ -0,0 +1,12 @@
+//Variable to access constants from constants module.
+const constans = require('./constants.js');
+
+//Variable to access generateREADME method from readme-template module.
+const readmeTemplace = require('./readme-template.js');
+
+//Variable to access inquirere module methods.
+const inquirer = require('inquirer');
+
+//Variable to access file system module methods.
+const fs = require('fs');
+
